bc3adb87ffd73d59706016edeb83a96a4b8f1d95
ManuVerger <mverger@frba.utn.edu.ar>
2024-06-14 22:40:43 -0300

    Vuelta

--- src/main/java/ar/edu/utn/dds/k3003/app/Fachada.java
+++ src/main/java/ar/edu/utn/dds/k3003/app/Fachada.java
@@ -28,24 +28,24 @@ public class Fachada implements ar.edu.utn.dds.k3003.facades.FachadaViandas {
         this.viandaMapper = new ViandaMapper();
     }
 
-//    @Override
-//    public ViandaDTO agregar(ViandaDTO viandaDTO) {
-//        Vianda vianda = new Vianda(viandaDTO.getCodigoQR(), viandaDTO.getFechaElaboracion(), viandaDTO.getEstado(), viandaDTO.getColaboradorId(), viandaDTO.getHeladeraId());
-//        vianda = this.viandaRepository.save(vianda);
-//        return viandaMapper.map(vianda);
-//    }
-
+    @Override
     public ViandaDTO agregar(ViandaDTO viandaDTO) {
-        // Verificar si el código QR ya existe
-        Optional<Vianda> existingVianda = Optional.ofNullable(viandaRepository.findByQR(viandaDTO.getCodigoQR()));
-        if (existingVianda.isPresent()) {
-            throw new IllegalArgumentException("El código QR ya existe.");
-        }
-
         Vianda vianda = new Vianda(viandaDTO.getCodigoQR(), viandaDTO.getFechaElaboracion(), viandaDTO.getEstado(), viandaDTO.getColaboradorId(), viandaDTO.getHeladeraId());
         vianda = this.viandaRepository.save(vianda);
         return viandaMapper.map(vianda);
     }
+
+//    public ViandaDTO agregar(ViandaDTO viandaDTO) {
+//        // Verificar si el código QR ya existe
+//        Optional<Vianda> existingVianda = Optional.ofNullable(viandaRepository.findByQR(viandaDTO.getCodigoQR()));
+//        if (existingVianda.isPresent()) {
+//            throw new IllegalArgumentException("El código QR ya existe.");
+//        }
+//
+//        Vianda vianda = new Vianda(viandaDTO.getCodigoQR(), viandaDTO.getFechaElaboracion(), viandaDTO.getEstado(), viandaDTO.getColaboradorId(), viandaDTO.getHeladeraId());
+//        vianda = this.viandaRepository.save(vianda);
+//        return viandaMapper.map(vianda);
+//    }
     @Override
     public ViandaDTO modificarEstado(String qr, EstadoViandaEnum nuevoEstado) {
         Vianda vianda = viandaRepository.findByQR(qr);
